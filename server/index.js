3bb9013a6ef635a40bbe9001c0d9b92c9b222aba
James C-D <james.changdavidson@gmail.com>
2021-01-19 00:06:42 -0800

    Added connection to MongoDB via env vars

--- server/index.js
+++ server/index.js
@@ -8,3 +8,13 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = process.env.MONGO_DB_URL;
+
+console.log(CONNECTION_URL);
+
+const SERVER_PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(SERVER_PORT, () => console.log(`Server running on port ${ SERVER_PORT }`)))
+    .catch((error) => console.error(error.message));
