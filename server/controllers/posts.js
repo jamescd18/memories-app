1ce94fa066d098fa567d23cec766abaf93fbfa6f
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-01-19 22:20:39 -0800

    Linked server model to controller for getPosts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,14 @@
-export const getPosts = (req, res) => {
-    res.send('Get Posts works');
+import { restart } from 'nodemon';
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 }
 
 export const createPost = (req, res) => {
